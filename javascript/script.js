cd98a248842572cc6bbbae19675448f21213a742
Maya N <nettathm@gmail.com>
2023-09-06 10:22:53 -0600

    made sure that all files were connected

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 $.ajax({
   type: "GET",
-  url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=dyJlprt5GV4U77gi63lcD1hjTcNSPTsi",
+  url: "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=dyJlprt5GV4U77gi63lcD1hjTcNSPTsi",
   async: true,
   dataType: "json",
   success: function (json) {
